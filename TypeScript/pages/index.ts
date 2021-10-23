173bd41c973433dcef79d8abee48728a1f05d3f4
rodrigo debian <correo.rodrigo@gmail.com>
2021-10-23 13:46:01 -0500

    adding files

--- TypeScript/pages/index.ts
+++ TypeScript/pages/index.ts
@@ -1,6 +1,6 @@
 // NOTE: make sure you import only WebSdk typings here, not a WebSdk code!
 // Also make sure this is not a NodeJS module. WebSdk is a browser-only library!
-import '../sdk/index'
+import '../typings/index';
 import { FingerprintReader } from '@digitalpersona/devices';
 
 export class FingerprintSigninControl {
