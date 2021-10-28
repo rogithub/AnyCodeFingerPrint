daa93d186f6beed397a40699d7f9cd129d2e4278
rodrigo debian <correo.rodrigo@gmail.com>
2021-10-28 11:29:21 -0500

    capture

--- TypeScript/pages/index.ts
+++ TypeScript/pages/index.ts
@@ -1,18 +1,28 @@
 // NOTE: make sure you import only WebSdk typings here, not a WebSdk code!
 // Also make sure this is not a NodeJS module. WebSdk is a browser-only library!
-import { FingerprintReader, SampleFormat } from '@digitalpersona/devices';
+import { FingerprintReader, SampleFormat, SamplesAcquired } from '@digitalpersona/devices';
+import { Utf8 } from '@digitalpersona/core';
 // https://hidglobal.github.io/digitalpersona-devices/tutorial.html
 
 // Event handlers.
 let onDeviceConnected = () => { console.log('called'); }
 let onDeviceDisconnected = () => { console.log('called'); };
 let onQualityReported = () => { console.log('called'); };
-let onSamplesAcquired = () => { console.log('called'); };
+let onSamplesAcquired = (data: SamplesAcquired) => {
+    const rawImages = data.samples.map(sample =>
+        btoa(Utf8.fromBase64Url(sample.Data))
+    );
+
+    const sampleImageUrl = 'data:image/png;base64,' + rawImages[0];
+    $("#huella").attr("src", sampleImageUrl);
+
+    return sampleImageUrl;
+};
 let onReaderError = () => { console.log('called'); };
 
 let init = async (r: FingerprintReader) => {
     try {
-        await r.startAcquisition(SampleFormat.Intermediate);
+        await r.startAcquisition(SampleFormat.PngImage);
     } catch (err) {
         console.error(err);
     }
